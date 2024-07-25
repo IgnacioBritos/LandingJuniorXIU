2fb70aac3edf6e1e07d46221ff8dce12c7ffa5b3
GasparBritos <gasparbritos2011@gmail.com>
2024-07-25 19:19:12 -0300

    Descripción de los cambios

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/docs/' // Asegúrate de que esto refleje la ruta base correcta
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
